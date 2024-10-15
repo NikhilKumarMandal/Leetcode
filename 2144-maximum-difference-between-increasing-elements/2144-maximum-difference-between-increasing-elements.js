17366b314e8160eb532fa133b944971e5c5d1d34
Nikhil <155902231+NikhilKumarMandal@users.noreply.github.com>
2024-10-15 21:30:36 +0530

    Time: 53 ms (71.83%), Space: 48.9 MB (76.59%) - LeetHub

---
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */ 
+var maximumDifference = function(nums) {
+    let minNum = nums[0]; 
+    let maxDiff = -1;      
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > minNum) {
+            maxDiff = Math.max(maxDiff, nums[i] - minNum);
+        } else {
+            minNum = nums[i];
+        }
+    }
+
+    return maxDiff
+};
