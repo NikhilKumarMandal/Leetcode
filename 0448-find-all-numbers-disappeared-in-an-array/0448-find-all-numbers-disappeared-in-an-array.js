4557465d4923a7fe6219d5d0e502d23083212025
Nikhil <155902231+NikhilKumarMandal@users.noreply.github.com>
2024-10-22 08:42:06 +0530

    Time: 21 ms (96.75%), Space: 61.4 MB (39.23%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -3,18 +3,13 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-     const n = nums.length;
+    const n = nums.length;
+    const set = new Set(nums);  
     const result = [];
 
-    for (let i = 0; i < n; i++) {
-        const index = Math.abs(nums[i]) - 1;
-        nums[index] = Math.abs(nums[index]) * -1;
-    }
-
-    
-    for (let i = 0; i < n; i++) {
-        if (nums[i] > 0) {
-            result.push(i + 1);
+    for (let i = 1; i <= n; i++) {
+        if (!set.has(i)) {
+            result.push(i); 
         }
     }
 
